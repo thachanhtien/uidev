9faf81d0b4352cfb5b97d5ba32f693290c80fee0
AT <thachanhtien.92@gmail.com>
2022-10-14 03:47:21 +0700

    Fix animation building

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const html = document.documentElement;
 const canvas = document.getElementById("images-render");
 const context = canvas.getContext("2d");
+const heightSection1 = $("#section1").innerHeight()
+const heightSection2 = $("#section2").innerHeight()
 
 setTimeout(() => {
   $(`#block1 .aos`).addClass("aos-animate");
@@ -46,37 +48,39 @@ const addAndRemoveClass = (blockId, classAdd, classRemove) => {
 window.addEventListener("scroll", () => {
   const scrollTop = html.scrollTop;
   const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const maxScrollSection1 = $("#section1").innerHeight();
 
   requestAnimationFrame(() => updateImage(frameIndex + 1));
-  const scrollFraction = scrollTop / maxScrollTop;
+  const scrollFraction = scrollTop / maxScrollSection1;
 
   const frameIndex = Math.min(
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
   );
-  if (frameIndex < 120) {
-    addAndRemoveClass("block1", "aos-animate", "aos-out");
-  } else {
-    addAndRemoveClass("block1", "aos-out", "aos-animate");
-  }
-  console.log(frameIndex)
 
-  if (frameIndex > 200 && frameIndex < 240) {
+
+  if (scrollTop > 50 && scrollTop < 550) {
+    addAndRemoveClass("section1", "aos-animate", "aos-out");
+  } else {
+    addAndRemoveClass("section1", "aos-out", "aos-animate");
+  }
+
+  if (scrollTop > heightSection1 - 400 && scrollTop < heightSection1) {
     $(`#block2 .aos`).addClass("aos-animate");
   } else {
     $(`#block2 .aos`).removeClass("aos-animate");
   }
-  if (frameIndex > 280 && frameIndex < 320) {
+  if (scrollTop > heightSection1 + 200 && scrollTop < heightSection1 + heightSection2 / 4) {
     $(`#block3 .aos`).addClass("aos-animate");
   } else {
     $(`#block3 .aos`).removeClass("aos-animate");
   }
-  if (frameIndex > 360 && frameIndex < 410) {
+  if (scrollTop > heightSection1 + heightSection2 / 4 + 200 && scrollTop < heightSection1 + heightSection2 / 4 + 600) {
     $(`#block4 .aos`).addClass("aos-animate");
   } else {
     $(`#block4 .aos`).removeClass("aos-animate");
   }
-  if (frameIndex > 440 && frameIndex < 490) {
+  if (scrollTop > heightSection1 + heightSection2 / 4 + 800 && scrollTop < heightSection1 + heightSection2 / 4 + 1200) {
     $(`#block5 .aos`).addClass("aos-animate");
   } else {
     $(`#block5 .aos`).removeClass("aos-animate");
